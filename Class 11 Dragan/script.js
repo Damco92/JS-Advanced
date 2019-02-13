2923f31f4a8f663df3625873f49a1dccf9c6daff
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-02-13 16:46:23 +0100

    Animal feeding exercise class 11 Dragan

--- Class 11 Dragan/script.js
+++ Class 11 Dragan/script.js
@@ -1,1 +1,44 @@
-alert('check');
+class Animal {
+  constructor(name, type, age, size, isEaten) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+    this.size = size;
+    this.isEaten = false;
+  }
+  get type() {
+    return this.type;
+  }
+  set type(typeInput) {
+    if (
+      typeInput === 'carnivore' ||
+      typeInput === 'herbivore' ||
+      typeInput === 'omnivore'
+    ) {
+      this.type = typeInput;
+    } else {
+      throw new Error('can not set this type of animal');
+    }
+  }
+
+  eat(food) {
+    if (food instanceof Animal) {
+      if (this.type === 'herbivore') {
+        console.log(
+          `The animal ${this.name} is herbivore and does not eat meat`
+        );
+      } else if (
+        this.type === 'carnivore' ||
+        (this.type === 'omnivore' && food.size * 2 < this.size)
+      ) {
+        console.log(`The animal ${this.name} is eating the ${food.name}`);
+        this.isEaten = true;
+      }
+    }
+  }
+}
+
+const myLion = new Animal('King', 'carnivore', 23, 10);
+const myRabbit = new Animal('Dusko', 'herbivore', 2);
+
+myLion.type;
