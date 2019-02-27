2b814f48ebd0f905f01735194870cb41f037071e
Damjan Stojanovski <damjan_stojanovski@hotmail.com>
2019-02-27 01:02:19 +0100

    last changes for 27th Feb

--- Starwars-APP/script/script.js
+++ Starwars-APP/script/script.js
@@ -7,29 +7,48 @@ $('#cargoLink').on('click', function() {
 $('#miningLink').on('click', function() {
   showMinerForm();
 });
-$('#fighterBtn').on('click', function() {
-  let fighters = [];
-  let fighterName = $('#fighterShipName').val();
-  let fighterPilot = $('#fighterPilot').val();
-  let fighterFuelCapacity = $('#fighter_fuel_tank_cappacity').val();
-  let fighterLocation = $('#currentLocation').val();
-  let fighterWeapons = $('#weapons').val();
-  let fighterShields = $('#shield').val();
-  let fighterKills = $('#num_of_kills').val();
+// $('#fighterBtn').on('click', function() {
+//   let fighters = [];
+//   let fighterName = $('#fighterShipName').val();
+//   let fighterPilot = $('#fighterPilot').val();
+//   let fighterFuelCapacity = $('#fighter_fuel_tank_cappacity').val();
+//   let fighterLocation = $('#currentLocation').val();
+//   let fighterWeapons = $('#weapons').val();
+//   let fighterShields = $('#shield').val();
+//   let fighterKills = $('#num_of_kills').val();
 
-  fighters.push(
-    new Fighter({
-      name: fighterName,
-      pilot: fighterPilot,
-      fuleTankCapacity: fighterFuelCapacity,
-      currentLocation: fighterLocation,
-      weapons: fighterWeapons,
-      shield: fighterShields,
-      NumberOfKills: fighterKills
-    })
-  );
+//   fighters.push(
+//     new Fighter({
+//       name: fighterName,
+//       pilot: fighterPilot,
+//       fuleTankCapacity: fighterFuelCapacity,
+//       currentLocation: fighterLocation,
+//       weapons: fighterWeapons,
+//       shield: fighterShields,
+//       NumberOfKills: fighterKills
+//     })
+//   );
+//   console.log(fighters);
+//   return fighters;
+// });
+
+$('#fighterBtn').on('click', function() {
+  // console.log('helooo')
+
+  let fighters = [];
+  let Ship = {
+    name: $('#fighterShipName').val(),
+    pilot: $('#fighterPilot').val(),
+    fuleTankCapacity: $('#fighter_fuel_tank_cappacity').val(),
+    currentLocation: $('#currentLocation').val(),
+    weapons: $('#weapons').val(),
+    shield: $('#shield').val(),
+    NumberOfKills: $('#num_of_kills').val()
+  };
+  console.log(Ship);
+
+  fighters.push(new Fighter(Ship));
   console.log(fighters);
-  return fighters;
 });
 
 $('#cargoBtn').on('click', function() {
